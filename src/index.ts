9d11580f481175bed1db756cb92101f7c0ae38de
Arun Geo Thomas <arungeothomas@gmail.com>
2026-09-26 14:33:53 +0200

    Remove unused static Worker entrypoint

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-type AssetsBinding = {
-  fetch(request: Request): Promise<Response>;
-};
-
-export default {
-  async fetch(request: Request, env: { ASSETS: AssetsBinding }): Promise<Response> {
-    return env.ASSETS.fetch(request);
-  },
-};
